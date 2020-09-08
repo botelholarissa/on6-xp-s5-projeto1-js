4c2fb7b576b4d21938aee6c7ac5adbe835e66fa3
botelholarissa <conta.larissa@gmail.com>
2020-09-07 22:00:10 -0300

    Receber via terminal as entradas id e quantidade

--- projeto/index.js
+++ projeto/index.js
@@ -2,11 +2,18 @@ console.log('--------------------------------------')
 console.log('     Projeto Carrinho de Compras     ')
 console.log('--------------------------------------')
 
+// Database e Readline
 const db = require('./database')
 const read = require('readline-sync')
 
+// Listar no console uma tabela contendo os produtos em ordem crescente de preço (do menor ao maior)
 const { produtos } = db
 
 produtos.sort((a, b) => a.preco - b.preco)
 
-console.table(produtos)
+console.table(produtos) 
+
+// Receber via terminal as entradas de id e quantidade dos produtos a serem adquiridos.
+
+read.question('Digite a id do produto desejado: \n')
+read.question('Digite a quantidade que gostaria de adquirir: \n')
