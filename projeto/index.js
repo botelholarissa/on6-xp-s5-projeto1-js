6783327afae59708610b16896fbd1c425b42815b
botelholarissa <conta.larissa@gmail.com>
2020-09-08 17:43:00 -0300

    Ainda não está funcionando

--- projeto/index.js
+++ projeto/index.js
@@ -15,5 +15,44 @@ console.table(produtos)
 
 // Receber via terminal as entradas de id e quantidade dos produtos a serem adquiridos.
 
-read.question('Digite a id do produto desejado: \n')
-read.question('Digite a quantidade que gostaria de adquirir: \n')
+let inputId = parseInt(read.question('Digite a id do produto desejado: '))
+let inputQty = parseInt(read.question('Digite a quantidade que gostaria de adquirir: '))
+
+let continuar = read.question('Você deseja continuar comprando? S ou N: ')
+
+let voucher = read.question('Você possui cupom de desconto? ')
+let hoje = new Date()
+let dataFormatada = hoje.toLocaleDateString('pt-BR')
+
+    
+class Pedido {
+    constructor(produtos, cupom, dataPedido) {
+    this.produtos = produtos
+    this.cupom = cupom
+    this.data = dataPedido
+    }
+    qtdTotal(){
+
+    }
+    vlrSubTotal(){
+
+    }
+    vlrDesconto(){
+
+    }
+    vlrTotal(){
+
+    }
+}   
+
+const pedido1 = new Pedido(inputId, inputQty, dataFormatada)
+console.log(pedido1)
+
+// Regra de continuar a comprar
+if (continuar == 1) {
+    return inputId, inputQty
+}else {
+    return voucher} 
+
+   // Perguntar se a cliente possui cupom de desconto.
+ 
