51ef41468ac2ac7600b5c5ab668380cf230cbf0e
botelholarissa <conta.larissa@gmail.com>
2020-09-07 21:50:52 -0300

    Listar tabela em ordem crescente de preço

--- projeto/index.js
+++ projeto/index.js
@@ -3,3 +3,10 @@ console.log('     Projeto Carrinho de Compras     ')
 console.log('--------------------------------------')
 
 const db = require('./database')
+const read = require('readline-sync')
+
+const { produtos } = db
+
+produtos.sort((a, b) => a.preco - b.preco)
+
+console.table(produtos)
