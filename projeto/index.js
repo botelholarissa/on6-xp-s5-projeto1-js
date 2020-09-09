cce8b9adac8aea9f76c55f538a0f2d7d582d3744
botelholarissa <conta.larissa@gmail.com>
2020-09-08 21:38:20 -0300

    Comprar mais

--- projeto/index.js
+++ projeto/index.js
@@ -13,19 +13,55 @@ produtos.sort((a, b) => a.preco - b.preco)
 
 console.table(produtos) 
 
-// Receber via terminal as entradas de id e quantidade dos produtos a serem adquiridos.
 
-let inputId = parseInt(read.question('Digite a id do produto desejado: '))
+// Receber via terminal as entradas de id e quantidade dos produtos a serem adquiridos.
+const carrinho = []
+
+const compra = () => {
+    const inputId = parseInt(read.question('Por favor, informe o id do produto desejado:'))
+
+    const inputQuantidade = parseInt(read.question('Agora informe a quantidade do produto desejado:'))
+
+    let desejaComprarMais = read.question('Deseja inserir mais algum produto no carrinho? Se sim digite 1, se não 2:')
+
+    let inputCupom = ('Possui algum cupom de desconto? Se sim, digite o valor:')
+
+    carrinho.push(inputId)
+
+    if (desejaComprarMais == 1) {
+        return compra()
+    } else {
+        return parseInt(read.question(inputCupom))
+    }
+
+
+}
+
+compra()
+
+console.log(carrinho)
+/* let inputId = parseInt(read.question('Digite a id do produto desejado: '))
 let inputQty = parseInt(read.question('Digite a quantidade que gostaria de adquirir: '))
 
-let continuar = read.question('Você deseja continuar comprando? S ou N: ')
+let continuar = JSON.stringify(read.question('Você deseja continuar comprando? S ou N: '))
 
-let voucher = read.question('Você possui cupom de desconto? ')
+let inputVoucher = read.question('Você possui cupom de desconto? ')
+
 let hoje = new Date()
 let dataFormatada = hoje.toLocaleDateString('pt-BR')
+console.log(continuar)
+// Regra de continuar a comprar (colocar dentro de um laço - rodar até "N")
+
+for (escolha === "S"; escolha  ) */
+/* if (continuar === "S") {
+    return inputId, inputQty
+} else {
+    return inputVoucher
+} */
+
 
     
-class Pedido {
+/* class Pedido {
     constructor(produtos, cupom, dataPedido) {
     this.produtos = produtos
     this.cupom = cupom
@@ -47,12 +83,8 @@ class Pedido {
 
 const pedido1 = new Pedido(inputId, inputQty, dataFormatada)
 console.log(pedido1)
+ */
 
-// Regra de continuar a comprar
-if (continuar == 1) {
-    return inputId, inputQty
-}else {
-    return voucher} 
 
    // Perguntar se a cliente possui cupom de desconto.
  
