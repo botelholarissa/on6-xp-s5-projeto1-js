cc8ca7a65b39598ecb32a84a38636ec24dfca352
botelholarissa <conta.larissa@gmail.com>
2020-09-11 18:10:44 -0300

    Criar class baseado no que vimos na aula de quarta

--- projeto/index.js
+++ projeto/index.js
@@ -2,132 +2,61 @@ console.log('--------------------------------------')
 console.log('     Projeto Carrinho de Compras     ')
 console.log('--------------------------------------')
 
-// Database e Readline
 const db = require('./database')
 const read = require('readline-sync')
 
-// Listar no console uma tabela contendo os produtos em ordem crescente de preço (do menor ao maior)
 const { produtos } = db
-
 produtos.sort((a, b) => a.preco - b.preco)
-
 console.table(produtos) 
 
-
-// Receber via terminal as entradas de id e quantidade dos produtos a serem adquiridos.
-const carrinho = []
-
-const compra = () => {
-    const inputId = parseInt(read.question('Por favor, informe o id do produto desejado:'))
-
-    const inputQuantidade = parseInt(read.question('Agora informe a quantidade do produto desejado:'))
-
-    let desejaComprarMais = read.question('Deseja inserir mais algum produto no carrinho? Se sim digite 1, se não 2:')
-
-    let inputCupom = parseInt(read.question('Possui algum cupom de desconto? Se sim, digite o valor:'))
-
-    carrinho.push(inputId)
-
-    if (desejaComprarMais == 1) {
-        return compra()
-    } else {
-        return parseInt(read.question(inputCupom))
+class Purchase {
+    constructor(){
+        this.products = []
+        this.quantity = []
+        this.subtotal = 0
+        this.discount = 0
+        this.total = 0
+        this.date = new Date().toLocaleDateString('pt-BR')
     }
 
+    addProduct(id, quantity) {
+        const product = produtos.filter(item => item.id === id)
+        if(typeof produto[0] != 'undefined') {
+            this.products.push(produto[0])
+            this.quantity.push(quantity)
+        } else {
+            console.log("Digite um produto existente na lista.")
+        }
+    }
 
+    calcSubtotal(){
+        for(let item = 0; item < this.produtos.length ; item++) 
+            this.subtotal =+ this.subtotal + (parseFloat(this.products[i].preco) * this.quantity[i])
+    }
+
+    calcDiscount(porcentagem) {
+        this.discount = this.subtotal * percentage/100
+    }
+
+    calcTotal() {
+        this.total = this.subtotal - this.discount
+    }
+
+    displayCart() {
+        let order = []
+        for(let item = 0; item < this.produtos.length; item++) {
+            pedido = {
+                id: this.produtos[item].id,
+                data: this.data,
+                nome: this.produtos[item].nome,
+                descricao: this.produtos[item].descricao,
+                categoria: this.produtos[item].categoria,
+                preco: this.produtos[item].preco,
+                quantidade: this.quantidade[item],
+            }
+            order.push(pedido)
+        }
+        console.table(order)
+    }
 }
 
-compra()
-
-/* let inputId = parseInt(read.question('Digite a id do produto desejado: '))
-let inputQty = parseInt(read.question('Digite a quantidade que gostaria de adquirir: '))
-
-let continuar = JSON.stringify(read.question('Você deseja continuar comprando? S ou N: '))
-
-let inputVoucher = read.question('Você possui cupom de desconto? ')
-
-let hoje = new Date()
-let dataFormatada = hoje.toLocaleDateString('pt-BR')
-console.log(continuar)
-// Regra de continuar a comprar (colocar dentro de um laço - rodar até "N")
-
-for (escolha === "S"; escolha  ) */
-/* if (continuar === "S") {
-    return inputId, inputQty
-} else {
-    return inputVoucher
-} */
-
-
-    
-/* class Pedido {
-    constructor(produtos, cupom, dataPedido) {
-    this.produtos = produtos
-    this.cupom = cupom
-    this.data = dataPedido
-    }
-    qtdTotal(){
-
-    }
-    vlrSubTotal(){
-
-    }
-    vlrDesconto(){
-
-    }
-    vlrTotal(){
-
-    }
-}   
-
-const pedido1 = new Pedido(inputId, inputQty, dataFormatada)
-console.log(pedido1)
- */
-
-
-   // Perguntar se a cliente possui cupom de desconto.
- 
-//pra fazer o valor total tem que fazer o reduce
-
-//const valorFinal []
-//const valorTotal = (acumulad)
-
-
-
-
-const db = require('./database')
-const read = require('readline-sync')
-
-// Listar no console uma tabela contendo os produtos em ordem crescente de preço (do menor ao maior)
-const { produtos } = db
-
-produtos.sort((a, b) => a.preco - b.preco)
-
-console.table(produtos) 
-
-
-// Receber via terminal as entradas de id e quantidade dos produtos a serem adquiridos.
-/* const carrinho = []
-
-const compra = () => {
-    const inputId = parseInt(read.question('Por favor, informe o id do produto desejado:'))
-
-    const inputQuantidade = parseInt(read.question('Agora informe a quantidade do produto desejado:'))
-
-    let desejaComprarMais = read.question('Deseja inserir mais algum produto no carrinho? Se sim digite 1, se não 2:')
-
-    let inputCupom = ('Possui algum cupom de desconto? Se sim, digite o valor:')
-
-    carrinho.push(inputId)
-
-    if (desejaComprarMais == 1) {
-        return compra()
-    } else {
-        return parseInt(read.question(inputCupom))
-    }
-
-
-}
-
-compra()
- */
